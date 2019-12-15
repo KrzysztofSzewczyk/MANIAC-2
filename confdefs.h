2af15b4045de61a9dcb1db16d8c83e6e39d36052
Krzysztof Szewczyk <kspalaiologos@gmail.com>
2019-12-15 13:16:37 +0100

    add core/makefile

---
+++ confdefs.h
@@ -0,0 +1,9 @@
+/* confdefs.h */
+#define PACKAGE_NAME "MANIAC-2"
+#define PACKAGE_TARNAME "maniac-2"
+#define PACKAGE_VERSION "0.1"
+#define PACKAGE_STRING "MANIAC-2 0.1"
+#define PACKAGE_BUGREPORT "kspalaiologos@gmail.com"
+#define PACKAGE_URL ""
+#define PACKAGE "maniac-2"
+#define VERSION "0.1"
---
+++ config.h
@@ -0,0 +1,26 @@
+/* config.h.  Generated from config.h.in by configure.  */
+/* config.h.in.  Generated from configure.ac by autoheader.  */
+
+/* Name of package */
+#define PACKAGE "maniac-2"
+
+/* Define to the address where bug reports for this package should be sent. */
+#define PACKAGE_BUGREPORT "kspalaiologos@gmail.com"
+
+/* Define to the full name of this package. */
+#define PACKAGE_NAME "MANIAC-2"
+
+/* Define to the full name and version of this package. */
+#define PACKAGE_STRING "MANIAC-2 0.1"
+
+/* Define to the one symbol short name of this package. */
+#define PACKAGE_TARNAME "maniac-2"
+
+/* Define to the home page for this package. */
+#define PACKAGE_URL ""
+
+/* Define to the version of this package. */
+#define PACKAGE_VERSION "0.1"
+
+/* Version number of package */
+#define VERSION "0.1"
