343f20fafd26b4f422b6c5c015618d0e2ad48e65
Krzysztof Szewczyk <kspalaiologos@gmail.com>
2019-12-17 16:16:25 +0100

    FEN

--- maniac/main.c
+++ maniac/main.c
@@ -4,6 +4,7 @@
 #include <stdio.h>
 #include <string.h>
 #include <ctype.h>
+#include <stdbool.h>
 
 int main(void) {
 	new_los_alamos(0);
@@ -16,7 +17,7 @@ struct los_alamos_t * new_los_alamos(int engine) {
 		return NULL;
 	
 	this->setup = "rnqknr/pppppp///PPPPPP/RNQKNR";
-	this->computercolor = 1;
+	this->computer_color = 1;
 	this->computer = new_engine();
 	
 	los_alamos_reset();
@@ -34,7 +35,7 @@ struct los_alamos_t * new_los_alamos(int engine) {
 	while(true) {
 		for(int x = 0; x < 6; x++) {
 			for(int y = 0; y < 6; y++)
-				putchar(los_alamos_piece_to_char(this->board[y][x]));
+				putchar(los_alamos_piece_to_char(this->board[x][y]));
 			
 			if(x != 5)
 				putchar('/');
@@ -42,16 +43,16 @@ struct los_alamos_t * new_los_alamos(int engine) {
 		
 		fflush(stdout);
 		
-		char l[6];
-		scanf("%5s", l);
+		char l[6], len;
+		len = scanf("%5s", l);
 		
-		if(strlen(l) < 4)
+		if(len < 4)
 			continue;
 		
 		int x2 = l[0]-'a', y2 = l[1]-'a', x1 = l[2]-'a', y1 = l[3]-'a';
 		int promote = 3;
 		
-		if(strlen(l) > 4) {
+		if(len > 4) {
 			promote = los_alamos_piece_to_char(toupper(l[4]));
 			
 			if(promote == 0)
@@ -62,4 +63,70 @@ struct los_alamos_t * new_los_alamos(int engine) {
 	}
 	
 	return this;
+}
+
+void los_alamos_reset(struct los_alamos_t * this) {
+	if(this->move_number > 0)
+		this->move_number--;
+	
+	for(int y = 0; y < 6; y++)
+		for(int x = 0; x < 6; x++)
+			this->board[x][y] = 0;
+	
+	los_alamos_load_fen(this);
+	
+	this->control = new_move_checker(this);
+}
+
+void los_alamos_load_fen(struct los_alamos_t * this) {
+	int parseplace = 0, x = 0, y = 5;
+	
+	while(parseplace < strlen(this->setup)) {
+		char c = this->setup[parseplace];
+		
+		if(c == '/') {
+			x = 0;
+			y--;
+		} else if(c == '-') {
+			board[x][y] = UNDEFINED;
+			x++;
+		} else if(isdigit(c) && c != '0') {
+			int skip = c - '0';
+			
+			if(parseplace + 1 < strlen(this->setup)) {
+				char next = this->setup[parseplace + 1];
+				
+				if(isdigit(next)) {
+					parseplace++;
+					skip = 10 * skip + next - '0';
+				}
+			}
+			
+			x += skip;
+		} else if(x < 6 && y > -1) {
+			this->board[x][y] = los_alamos_char_to_piece(c);
+			x++;
+		}
+		
+		parseplace++;
+	}
+}
+
+int los_alamos_char_to_piece(int c) {
+	const static int magic[] = {
+		0x0AA6, 0xA824, 0xAAAA, 0xAAAA, 0xAAAA,
+		0xAAAA, 0xAAAA, 0xAAAA, 0x1AA7, 0xA935
+	};
+	
+	c -= 75;
+
+	if(c > 39 || ((c >> 2) > 1 && (c >> 2) < 8))
+		return 0xA;
+
+    switch(c & 3) {
+		case 0: return magic[c >> 2] >> 12;
+		case 1: return (magic[c >> 2] & 0x0F00) >> 8;
+		case 2: return (magic[c >> 2] & 0x00F0) >> 4;
+		case 3: return (magic[c >> 2] & 0x000F);
+	}
 }
--- maniac/main.h
+++ maniac/main.h
@@ -17,6 +17,20 @@ struct los_alamos_t {
 	char * setup;
 };
 
+#define BLACK_KING 0
+#define WHITE_KING 1
+#define BLACK_QUEEN 2
+#define WHITE_QUEEN 3
+#define BLACK_ROOK 4
+#define WHITE_ROOK 5
+#define BLACK_KNIGHT 6
+#define WHITE_KNIGHT 7
+#define BLACK_PAWN 8
+#define WHITE_PAWN 9
+#define UNDEFINED 10
+
 struct los_alamos_t * new_los_alamos(int engine);
+void los_alamos_reset(struct los_alamos_t * this);
+void los_alamos_load_fen(struct los_alamos_t * this);
 
 #endif
