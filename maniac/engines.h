01e736aee1ae2e9bb9040cf34f7888415fc1ab87
Krzysztof Szewczyk <kspalaiologos@gmail.com>
2019-12-17 20:58:50 +0100

    Parent's not needed

--- maniac/engines.h
+++ maniac/engines.h
@@ -2,8 +2,8 @@
 #ifndef ENGINES_H
 #define ENGINES_H
 
-extern struct move_t materialistic_engine(struct los_alamos_t, struct move_checker_t, int);
+extern struct move_t materialistic_engine(struct move_checker_t, int);
 
-extern struct move_t random_engine(struct los_alamos_t, struct move_checker_t, int);
+extern struct move_t random_engine(struct move_checker_t, int);
 
 #endif
--- maniac/main.h
+++ maniac/main.h
@@ -10,7 +10,7 @@ struct los_alamos_t {
 	int board[6][6];
 	
 	struct move_checker_t control;
-	struct move_t (*computer)(struct los_alamos_t, struct move_checker_t, int);
+	struct move_t (*computer)(struct move_checker_t, int);
 	
 	int computer_color;
 	int move_number;
