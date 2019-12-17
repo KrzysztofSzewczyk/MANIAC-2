6a818ea6abf2f7a224d10e18db3b10cca56e8685
Krzysztof Szewczyk <kspalaiologos@gmail.com>
2019-12-17 21:12:45 +0100

    Switch to board representation instead

--- maniac/move_checker.h
+++ maniac/move_checker.h
@@ -4,9 +4,13 @@
 
 #include "main.h"
 
+#include <stdbool.h>
+
 struct move_checker_t {
+	bool white_short_castling, black_short_castling;
+	bool white_long_castling, black_long_castling;
 };
 
-struct move_checker_t * new_move_checker(struct los_alamos_t * parent);
+struct move_checker_t * new_move_checker(int board[6][6]);
 
 #endif
