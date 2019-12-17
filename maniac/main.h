025e81de876a0078d436d41e5bd849c994390389
Krzysztof Szewczyk <kspalaiologos@gmail.com>
2019-12-17 20:48:58 +0100

    Move piece definitions to another file

--- maniac/main.h
+++ maniac/main.h
@@ -3,6 +3,7 @@
 #define MAIN_H
 
 #include "move_checker.h"
+#include "pieces.h"
 
 struct los_alamos_t {
 	int board[6][6];
@@ -15,18 +16,6 @@ struct los_alamos_t {
 	
 	char * setup;
 };
-
-#define BLACK_KING 0
-#define WHITE_KING 1
-#define BLACK_QUEEN 2
-#define WHITE_QUEEN 3
-#define BLACK_ROOK 4
-#define WHITE_ROOK 5
-#define BLACK_KNIGHT 6
-#define WHITE_KNIGHT 7
-#define BLACK_PAWN 8
-#define WHITE_PAWN 9
-#define UNDEFINED 10
 
 struct los_alamos_t * new_los_alamos(int engine);
 void los_alamos_reset(struct los_alamos_t * this);
---
+++ maniac/pieces.h
@@ -0,0 +1,17 @@
+
+#ifndef PIECES_H
+#define PIECES_H
+
+#define BLACK_KING 0
+#define WHITE_KING 1
+#define BLACK_QUEEN 2
+#define WHITE_QUEEN 3
+#define BLACK_ROOK 4
+#define WHITE_ROOK 5
+#define BLACK_KNIGHT 6
+#define WHITE_KNIGHT 7
+#define BLACK_PAWN 8
+#define WHITE_PAWN 9
+#define UNDEFINED 10
+
+#endif
