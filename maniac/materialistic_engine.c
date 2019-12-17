6d8a41ca8dc3de9c5ae85788bbdcb9bfaad3ab49
Krzysztof Szewczyk <kspalaiologos@gmail.com>
2019-12-17 21:25:55 +0100

    Engine stubs

--- maniac/materialistic_engine.c
+++ maniac/materialistic_engine.c
@@ -1,2 +1,7 @@
 
+#include "move_checker.h"
 
+// WIP
+
+struct move_t * materialistic_engine(struct move_checker_t * c, int computer_color) {
+}
--- maniac/random_engine.c
+++ maniac/random_engine.c
@@ -1,2 +1,7 @@
 
+#include "move_checker.h"
 
+// WIP
+
+struct move_t * random_engine(struct move_checker_t * c, int computer_color) {
+}
