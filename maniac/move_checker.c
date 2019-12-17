e5c354ba12ae807de40acc84bfb80db7ff33a49e
Krzysztof Szewczyk <kspalaiologos@gmail.com>
2019-12-17 21:00:19 +0100

    Accidentally copied over wrong prototype

--- maniac/move_checker.c
+++ maniac/move_checker.c
@@ -1,2 +1,2 @@
 
-
+#include "move_checker.h"
--- maniac/move_checker.h
+++ maniac/move_checker.h
@@ -2,7 +2,11 @@
 #ifndef MOVE_CHECKER_H
 #define MOVE_CHECKER_H
 
+#include "main.h"
+
 struct move_checker_t {
 };
+
+struct move_checker_t * new_move_checker(struct los_alamos_t * parent);
 
 #endif
