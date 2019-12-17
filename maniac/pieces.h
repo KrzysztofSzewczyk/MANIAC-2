59c414a1f39f46b885ec0c120820b1bab5d15bfb
Krzysztof Szewczyk <kspalaiologos@gmail.com>
2019-12-17 21:22:08 +0100

    Confused the order, wrap it up real fast

--- maniac/pieces.h
+++ maniac/pieces.h
@@ -2,16 +2,16 @@
 #ifndef PIECES_H
 #define PIECES_H
 
-#define BLACK_KING 0
-#define WHITE_KING 1
-#define BLACK_QUEEN 2
-#define WHITE_QUEEN 3
-#define BLACK_ROOK 4
-#define WHITE_ROOK 5
-#define BLACK_KNIGHT 6
-#define WHITE_KNIGHT 7
-#define BLACK_PAWN 8
-#define WHITE_PAWN 9
+#define BLACK_KING 1
+#define WHITE_KING 0
+#define BLACK_QUEEN 3
+#define WHITE_QUEEN 2
+#define BLACK_ROOK 5
+#define WHITE_ROOK 4
+#define BLACK_KNIGHT 7
+#define WHITE_KNIGHT 6
+#define BLACK_PAWN 9
+#define WHITE_PAWN 8
 #define UNDEFINED 10
 
 #endif
