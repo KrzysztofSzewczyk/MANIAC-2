2965c4fa5f56c379bc26e448a09847a61ed37052
Krzysztof Szewczyk <kspalaiologos@gmail.com>
2019-12-17 21:42:58 +0100

    Move checker cloning

--- maniac/move_checker.c
+++ maniac/move_checker.c
@@ -1,6 +1,8 @@
 
 #include "pieces.h"
 #include "move_checker.h"
+#include "board_state.h"
+#include "move.h"
 
 #include <stdlib.h>
 
@@ -33,6 +35,31 @@ struct move_checker_t * new_move_checker(int board[6][6]) {
 	for(int i = 0; i < 6; i++)
 		if(board[i][5] == BLACK_KING)
 			this->kingrb = i;
+	
+	return this;
+}
+
+struct move_checker_t * new_move_checker_clone(struct move_checker_t * clone) {
+	struct move_checker_t * this = malloc(sizeof(struct move_checker_t));
+	
+	if(!this)
+		return NULL;
+	
+	for(int i = 0; i < 6; i++)
+		for(int j = 0; j < 6; j++)
+			this->board[i][j] = clone->board[i][j];
+	
+	this->white_short_castling = clone->white_short_castling;
+	this->black_short_castling = clone->black_short_castling;
+	
+	this->white_long_castling = clone->white_long_castling;
+	this->black_long_castling = clone->black_long_castling;
+	
+	this->last_player_moved = clone->last_player_moved;
+	this->last_move = new_move_coord(
+		clone->last_move->start_col, clone->last_move->start_row,
+		clone->last_move->end_col, clone->last_move->end_row
+	);
 	
 	return this;
 }
