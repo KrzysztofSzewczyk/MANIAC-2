12dc8f869714b457947039ba841a8e91fd3ccc05
Krzysztof Szewczyk <kspalaiologos@gmail.com>
2019-12-18 21:32:17 +0100

    Knight move checking (dead simple!)

--- maniac/move_checker.c
+++ maniac/move_checker.c
@@ -149,3 +149,7 @@ bool move_checker_legal_rook_move(struct move_checker_t * this, int x1, int y1, 
 	} else
 		return false;
 }
+
+bool move_checker_legal_knight_move(struct move_checker_t * this, int x1, int y1, int x2, int y2) {
+	return abs((x1 - x2) * (y1 - y2)) == 2;
+}
