9e01a5488c825e0f73514eb62d650c43af72e982
Krzysztof Szewczyk <kspalaiologos@gmail.com>
2019-12-19 20:45:18 +0100

    player -> last_player_moved

--- maniac/move_checker.c
+++ maniac/move_checker.c
@@ -274,11 +274,11 @@ bool move_checker_pawn_promotion(struct move_checker_t * this, int x1, int y1, i
 }
 
 void move_checker_set_last_player_moved(struct move_checker_t * this, int player) {
-	this->player = last_player_moved;
+	this->last_player_moved = player;
 }
 
 int move_checker_get_last_player_moved(struct move_checker_t * this) {
-	return this->player;
+	return this->last_player_moved;
 }
 
 int move_checker_piece_owner(int piece) {
